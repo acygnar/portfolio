8a170780bd6a4256bc3fc6a5790311aff4197c56
Adrian C <adek2222@hotmail.com>
2019-02-16 14:21:41 +0100

    edit js script

--- script/script.js
+++ script/script.js
@@ -1,37 +1,48 @@
-const burger=document.querySelector(".burger");
-const leftSide=document.querySelector(".left-side");
-const rightSide=document.querySelector(".right-side");
-const navList=document.querySelector(".nav");
-const menu=document.querySelector(".menu");
-const skillsDescription=document.querySelector(".skills-description");
-const skills=document.querySelectorAll(".skill");
+const burger = document.querySelector(".burger");
+const leftSide = document.querySelector(".left-side");
+const rightSide = document.querySelector(".right-side");
+const navList = document.querySelector(".nav");
+const menu = document.querySelector(".menu");
+const skillsDescription = document.querySelector(".skills-description");
+const skills = document.querySelectorAll(".skill");
+const spacer = document.querySelector(".section-spacer");
+const gallery = document.querySelectorAll(".image");
 
 
 
 
-function activeBurger(){
+function activeBurger() {
     burger.classList.toggle("active");
     leftSide.classList.toggle("active");
     rightSide.classList.toggle("active");
     navList.classList.toggle("active");
     menu.classList.toggle("active");
 }
-burger.addEventListener("click",activeBurger);
+burger.addEventListener("click", activeBurger);
 
-function addAnimation(){
-    const scrollSize= window.scrollY;
-    const skillsPosition=skillsDescription.offsetTop;
-    if(scrollSize>skillsPosition){
-        for(i=0;i<=skills.length;i++){
+function addSlideAnimation() {
+    const scrollSize = window.scrollY;
+    const skillsPosition = skillsDescription.offsetTop;
+    if (scrollSize > skillsPosition) {
+        for (i = 0; i <= skills.length; i++) {
             skills[i].classList.add("active");
-            }
+        }
+    } else {
+        for (i = 0; i <= skills.length; i++) {
+            skills[i].classList.remove("active");
         }
     }
+}
 
-window.addEventListener("scroll",addAnimation);
+window.addEventListener("scroll", addSlideAnimation);
 
-
-
-
-
-    
+function galleryAppear(){
+    const scrollSize = window.scrollY;
+    if (scrollSize > 100) {
+        spacer.classList.add("active")
+        for (i = 0; i <= gallery.length; i++) {
+            gallery[i].classList.add("active");
+        }
+    }
+}
+window.addEventListener("scroll", galleryAppear);
